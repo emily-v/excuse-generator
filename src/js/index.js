5ff34ba56ccdefef063c03d870c2243e5bdad7e6
Emily Vong <emily.vong25@gmail.com>
2018-06-02 00:41:14 +0000

    initial commit with excuse generator

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,26 @@ import 'bootstrap';
 import '../style/index.scss';
 
 window.onload = function(){
-    console.log('Hello Rigo from the console!');
+    
+    //console.log('Hello Rigo from the console!');
     document.querySelector('.error').style.display = "none";
+    
+    var first = 'A ';
+    var adj = ['giant ','crazy ','loud '];
+    var noun = ['tourist ','airplane ','waterfall '];
+    var action = ['threw ','kicked ', 'smashed '];
+    var my = 'my ';
+    var item = ['backpack ','sunglasses ','shoes '];
+    var location = ['in the garage.','on the train.','in the grocery store.'];
+    
+    var rdmAdj = Math.floor(Math.random()*adj.length);
+    var rdmNoun = Math.floor(Math.random()*noun.length);
+    var rdmAction = Math.floor(Math.random()*action.length);
+    var rdmItem = Math.floor(Math.random()*item.length);
+    var rdmLocation = Math.floor(Math.random()*location.length);
+    
+    var finalSentence = first + adj[rdmAdj] + noun[rdmNoun] + action[rdmAction] + my + item[rdmItem] + location[rdmLocation];
+    
+    document.querySelector("#excuse").innerHTML = finalSentence;
+    
 };
